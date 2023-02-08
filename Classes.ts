6b5196dfe63309f46776599bc16717f2c99093f3
Azat <122405130+Depth-monster@users.noreply.github.com>
2023-02-08 06:28:12 +0300

    Update Classes.ts

--- Classes.ts
+++ Classes.ts
@@ -1,1 +1,72 @@
 
+
+```ts
+class Me {
+  name:string;
+  age:number;
+  nick:string;
+
+  constructor(name:string,age:number,nick:string){
+    this.name=name;
+    this.age=age;
+    this.nick=nick
+  }
+}
+
+const Azat = new Me('Azat',22,'pezda');
+console.log(Azat)
+```
+
+
+
+<details><summary><b>Output</b></summary>
+
+```ts
+"use strict";
+class Me {
+    constructor(name, age, nick) {
+        this.name = name;
+        this.age = age;
+        this.nick = nick;
+    }
+}
+const Azat = new Me('Azat', 22, 'pezda');
+console.log(Azat);
+
+```
+
+
+</details>
+
+
+<details><summary><b>Compiler Options</b></summary>
+
+```json
+{
+  "compilerOptions": {
+    "strict": true,
+    "noImplicitAny": true,
+    "strictNullChecks": true,
+    "strictFunctionTypes": true,
+    "strictPropertyInitialization": true,
+    "strictBindCallApply": true,
+    "noImplicitThis": true,
+    "noImplicitReturns": true,
+    "alwaysStrict": true,
+    "esModuleInterop": true,
+    "declaration": true,
+    "experimentalDecorators": true,
+    "emitDecoratorMetadata": true,
+    "target": "ES2017",
+    "jsx": "react",
+    "module": "ESNext",
+    "moduleResolution": "node"
+  }
+}
+```
+
+
+</details>
+
+**Playground Link:** [Provided](https://www.typescriptlang.org/play?#code/MYGwhgzhAECyCm0DeAoa0B2YC28BcEALgE4CWGA5gNxrRgX4YCu2ARvMTehqcANYES5ailrAA9hiLEmwQuOIAKLLkFlKAGnqMW7Yhp781wgJSp06QgAtSEAHQr4AXkdcL123e1PtbyzftDPhdePloAXxRIlAkpQmgAQQAvMHinTHgAdzh4RQByZNS8jQAmEo08gAd4JIATMDyTGliIcRB4OxBxCkVCwhMgA)
+      
